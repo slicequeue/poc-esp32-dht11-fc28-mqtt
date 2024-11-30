cdbf3deb964e294f80dded9de67084da3eee571b
slicequeue <slicequeue@gmail.com>
2024-11-30 13:23:40 +0900

    feat: JSON 데이터 표시 256바이트 이내로 줄임, 오류처리 추가

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ WiFiClient espClient;
 PubSubClient client(espClient);
 
 unsigned long previousMillis = 0;
-const unsigned long interval = 15 * 1000;// 10 * 60 * 1000; // 주기 : 10분에 한번씩
+const unsigned long interval = 5 * 1000;// 10 * 60 * 1000; // 주기 : 10분에 한번씩
 
 #define DHT_PIN 15     // 온습도 센서가 연결된 핀 번호
 #define DHT_TYPE DHT11 // 온습도 센서 타입
@@ -43,17 +43,21 @@ void loop()
     // 시간 기준
     String datetime = get_current_datetime_iso8601();
 
-    // 습도 단위: %
+    // 센서 데이터 읽기
     float humidityPercent = dht.readHumidity();
-    String jsonHumidityPercent = "{\"type\": \"humidity\", \"value\": " + String(humidityPercent) + ", \"unit\": \"%\", \"at\": \"" + datetime +"\"}";
-    // 온도 단위: 도씨
-    float temperatureCelsius = dht.readTemperature();
-    String jsonTemperatureCelsius = "{\"type\": \"temperature\", \"value\": " + String(temperatureCelsius) + ", \"unit\": \"'C\", \"at\": \"" + datetime + "\"}";
-    // 토양 습윤 단위: %
-    float soilMoisturePercent = map(analogRead(FC28_PIN), 4095, 0, 0, 100);
-    String jsonSoilMoisturePercent = "{\"type\": \"soil-moisture\", \"value\": " + String(soilMoisturePercent) + ", \"unit\": \"%\", \"at\": \"" + datetime + "\"}";
+    if (isnan(humidityPercent)) humidityPercent = -1; // 오류 시 기본값
 
-    String jsonTotal = "[" + jsonHumidityPercent + "," + jsonTemperatureCelsius + "," + jsonSoilMoisturePercent + "]";
+    float temperatureCelsius = dht.readTemperature();
+    if (isnan(temperatureCelsius)) temperatureCelsius = -1; // 오류 시 기본값
+
+    float soilMoisturePercent = map(analogRead(FC28_PIN), 4095, 0, 0, 100);
+
+    // JSON 메시지 생성 (크기 최적화) - broker 서버 현재는 256 바이트로 용량 제한이 있어 json 작성시 주의!
+    String jsonTotal = "{\"at\":\"" + datetime + "\",\"data\":["
+                       "{\"type\":\"humidity\",\"value\":" + String(humidityPercent) + ",\"unit\":\"%\"},"
+                       "{\"type\":\"temperature\",\"value\":" + String(temperatureCelsius) + ",\"unit\":\"C\"},"
+                       "{\"type\":\"soil-moisture\",\"value\":" + String(soilMoisturePercent) + ",\"unit\":\"%\"}"
+                       "]}";
     Serial.println(jsonTotal);
 
     client.publish(mqtt_topic_output, jsonTotal.c_str());
